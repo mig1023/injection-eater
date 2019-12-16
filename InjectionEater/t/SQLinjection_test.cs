cb471078944fd5be5a39665b6e7d6f816b119038
mig1023 <mig1023@yandex.ru>
2019-12-16 20:58:09 +0300

    test start

---
+++ InjectionEater/t/SQLinjection_test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace InjectionEater.t
+{
+    [TestFixture]
+    class SQLinjection_test
+    {
+        [Test]
+        public void SimpleSQLinject_test()
+        {
+            string simpleInject = @"' UNION SELECT username, password FROM users--";
+            bool test = SQLinjection.Eat(ref simpleInject);
+            Assert.AreEqual(test, true);
+        }
+    }
+}
